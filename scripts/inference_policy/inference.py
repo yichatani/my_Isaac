722ac35976adf12ee47959dcebe8a8e9787f63e2
Yicheng Ma <yichatma@gmail.com>
2025-05-15 00:55:48 +0800

    Update con_unet1d
    Co-Authored-By: Anisama19 <85790526+Anisama19@users.noreply.github.com>

--- scripts/inference_policy/inference.py
+++ scripts/inference_policy/inference.py
@@ -67,7 +67,7 @@ def inference_policy(data_sample,obs_steps=3,action_steps=6):
     Returns:
         np.ndarray: The predicted action as a numpy array.
     """
-    ckpt_path = pathlib.Path(ROOT_PATH + "/checkpoints/baseline_cube.ckpt")
+    ckpt_path = pathlib.Path(ROOT_PATH + "/checkpoints/cube_0515.ckpt")
     assert ckpt_path.is_file(), f"Checkpoint not found: {ckpt_path}"
 
     model = load_model_from_ckpt(ckpt_path)
